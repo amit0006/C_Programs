bbf06408dbd7b7e025f2fed938cd7a6d16920939
Amit Kumar Thakur <kumaramitthakur563@gmail.com>
2024-09-09 22:07:15 +0530

    Update calendar.c

--- calendar.c
+++ calendar.c
@@ -19,13 +19,6 @@ void create()
 		printf("Enter the description for that\n");
 		scanf("%s", c[i].description);
 	}
-	/*printf("Enter the date of these days\n");
-	for(int i=0;i<7;i++)
-		scanf("%d", &c[i].date);
-	printf("Enter the description of the activity that you have done on that day\n");
-	for(int i=0;i<7;i++)
-		scanf("%s", c[i].description);*/
-	//printf("Hello");
 }
 void display()
 {
@@ -36,32 +29,12 @@ void display()
 		printf("%d-", c[i].date);
 		printf("%s\n", c[i].description);
 	}
-	/*printf("The yoou have entered for these days respectively are\n");
-	for(int i=0;i<7;i++)
-		printf("%d\n", c[i].date);
-	printf("The description you have entered on these days respectively are \n");
-	for(int i=0;i<7;i++)
-		puts(c[i].description);*/
 }
 void main()
 {
 	create();
 	printf("Now displaying\n");
 	display();
-	/*struct calendar c;
-	printf("Enter the first day\n");
-	gets(c.day);
-	printf("Enter the date for that\n");
-	scanf("%d", &c.date);
-	printf("Enter description\n");
-	printf("Going to take the value\n");
-	//fflush(stdin);
-	scanf("%s",c.description);
-	printf("The day is \n");
-	puts(c.day);
-	printf("%d", c.date);
-	printf("The descrption is\n");
-	pu0ts(c.description);*/
 }
 
 
