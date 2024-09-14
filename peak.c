fb238c119f736d1d112d772295aaac045096d85d
Amit Kumar Thakur <kumaramitthakur563@gmail.com>
2024-09-15 00:34:28 +0530

    Add files via upload

---
+++ peak.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+void main()
+{
+	int a[100],n;
+	printf("Enter the size of the array\n");
+	scanf("%d", &n);
+	printf("Enter the elements to the array\n");
+	for(int i=0;i<n;i++)
+	{
+		scanf("%d", &a[i]);
+	}
+	printf("The array elements you have entered are\n");
+	for(int i=0;i<n;i++)
+	{
+		printf("%d\n", a[i]);
+	}
+	int l=0;
+	int h=n-1;
+	int mid;
+	while(l<=h)
+	{
+		mid=(l+h)/2;
+		if(a[mid]<a[mid+1])
+		{
+			l=mid+1;
+		}
+		else
+		{
+			h=mid-1;
+		}
+	}
+	printf("The index of the peak element is %d\n", l);
+}
