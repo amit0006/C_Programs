d89d3dd17ab7156efdf91e15558d668e7049c6d2
Amit Kumar Thakur <kumaramitthakur563@gmail.com>
2024-10-20 22:05:48 +0530

    Update postfix.c

--- postfix.c
+++ postfix.c
@@ -1,3 +1,7 @@
+/*Develop a Program in C for converting an Infix Expression to Postfix Expression. Program 
+should support for both parenthesized and free parenthesized 
+expressions with the operators: +, -, *, /, % (Remainder), ^ (Power) and alphanumeric 
+operands. */
 #include<stdio.h>
 #include<ctype.h>
 #include<string.h>
